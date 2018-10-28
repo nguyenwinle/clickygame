a958845da82501b77fe7e6c706853790a5a5a24a
Nguyen Le <ucnguyenle@gmail.com>
2018-10-27 21:13:03 -0700

    add files/edit app.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,102 @@
 import React, { Component } from "react";
 import FriendCard from "./components/FriendCard";
+import Nav from "./components/Nav";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import friends from "./friends.json";
 import "./App.css";
 
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
 class App extends Component {
-  // Setting this.state.friends to the friends json array
+  // Set this.state
   state = {
-    friends
+    friends,
+    currentScore: 0,
+    topScore: 0,
+    win: "",
+    lose: "",
+    clicked: [],
   };
 
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.handleReset();
+    }
   };
 
-  // Map over this.state.friends and render a FriendCard component for each friend object
+  handleIncrement = () => {
+    const newScore = this.state.currentScore + 1;
+    this.setState({
+      currentScore: newScore,
+      win: "",
+      lose: ""
+    });
+    if (newScore >= this.state.topScore) {
+      this.setState({ topScore: newScore });
+    }
+    else if (newScore === 15) {
+      this.setState({ win: "You win!" });
+    }
+    this.handleShuffle();
+  };
+
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      topScore: this.state.topScore,
+      lose: "You lost, Start Over",
+      clicked: []
+    });
+    this.handleShuffle();
+  };
+
+  handleShuffle = () => {
+    let shuffledFriends = shuffle(friends);
+    this.setState({ friends: shuffledFriends });
+  };
+
   render() {
     return (
       <Wrapper>
-        <Title>Friends List</Title>
-        {this.state.friends.map(friend => (
-          <FriendCard
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
-          />
-        ))}
+        <Nav
+          title="Clicky Game"
+          score={this.state.currentScore}
+          topScore={this.state.topScore}
+          win={this.state.win}
+          lose={this.state.lose}
+        />
+
+        <br/>
+        <br/>
+
+        <Title>
+          Click on each picture only once!
+        </Title>
+
+            {this.state.friends.map(friend => (
+              
+                <FriendCard
+                  key={friend.id}
+                  handleClick={this.handleClick}
+                  handleIncrement={this.handleIncrement}
+                  handleReset={this.handleReset}
+                  handleShuffle={this.handleShuffle}
+                  id={friend.id}
+                  image={friend.image}
+                />
+            
+            ))}
+       
       </Wrapper>
     );
   }
